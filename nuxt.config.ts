08da5619312df6fbe052ed03f62d5928d2d25f7d
KamenSentai <cvt.alain@gmail.com>
2022-09-01 18:05:19 +0200

    feat(responsive): add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,9 @@ export default defineNuxtConfig({
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+      ],
     },
   },
   css: [
