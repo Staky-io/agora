841b134775e7d132f364949d07a8640fab48771c
KamenSentai <cvt.alain@gmail.com>
2022-09-08 18:05:17 +0200

    feat(seo): add metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,15 @@ import commonjs from '@rollup/plugin-commonjs'
 
 const isDev = process.env.NODE_ENV === 'development'
 
+const parseTwitter = (twitter: string): string => {
+  try {
+    const url = new URL(twitter)
+    return `@${url.pathname.split('/').filter(Boolean)[0]}`
+  } catch (error) {
+    return `@${twitter}`
+  }
+}
+
 export default defineNuxtConfig({
   publicRuntimeConfig: {
     name: process.env.APP_NAME,
@@ -17,9 +26,20 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      title: 'Agora',
+      title: `${process.env.APP_NAME} - Agora governance`,
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: `Welcome to the Agora governance page of ${process.env.APP_NAME}.` },
+        { name: 'twitter:title', content: `${process.env.APP_NAME} - Agora governance` },
+        { name: 'twitter:url', content: isDev || !process.env.APP_URL ? 'http://localhost:3000/' : process.env.APP_URL },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'twitter:site', content: parseTwitter(process.env.APP_TWITTER) },
+        { name: 'twitter:image', content: '/agora.png' },
+        { name: 'twitter:description', content: `Welcome to the Agora governance page of ${process.env.APP_NAME}.` },
+        { name: 'og:url', content: isDev || !process.env.APP_URL ? 'http://localhost:3000/' : process.env.APP_URL },
+        { name: 'og:title', content: `${process.env.APP_NAME} - Agora governance` },
+        { name: 'og:description', content: `Welcome to the Agora governance page of ${process.env.APP_NAME}.` },
+        { name: 'og:image', content: '/agora.png' },
       ],
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
